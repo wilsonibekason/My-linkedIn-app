2d47b9c18ddb127b9d33fdc869dc07fd62172397
wilsonibekason <wanbekgithub@gmail.com>
2022-04-21 03:09:21 -0700

    added login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import { Login } from './components';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello world </h1>
-    </div>
+    
+     <Router>
+       <div className="App">
+       <Routes>
+         <Route path='/' element={<Login/>} />
+       </Routes>
+       </div>
+     </Router>
   );
 }
 
