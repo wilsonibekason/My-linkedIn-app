bd943ac2bb77498a138a90110367e1379a484e57
wilsonibekason <wanbekgithub@gmail.com>
2022-04-21 07:03:07 -0700

    added bottom bar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => {
   background-color: white;
   border-bottom: 1px solid rgba( 0,0,0,.08);
   left: 0;
-  padding: 0 24px;
+  paddsing: 0 24px;
+  padding-left: 24px;
+  padding-right: 24px;
   position: fixed;
   top: 0;
   width: 100vw;
@@ -66,8 +68,11 @@ const Header = () => {
   `;
    
   const Nav = styled.nav`
-     margin-left: auto;
+     maregin-left: auto;
      display: block;
+     @media screen and (max-width: 2000px){
+      padding-right: 5rem;
+     }
      @media screen and (max-width: 768px) {
        position: fixed;
        left: 0;
@@ -80,11 +85,108 @@ const Header = () => {
      display: flex;
      flex-wrap: nowrap;
      list-style-type: none;
+
+     .active{
+       span:after{
+         content: '';
+         transform: scaleX(1);
+         border-bottom: 2px solid #fff;
+         bottom: 0;
+         left: 0;
+         position: absolute;
+         border-color: rgba(0,0,0,0.9);
+         width: 100%;
+       }
+     }
    `;
 
    const  NavList = styled.li`
-     
-   `
+     display: flex;
+     align-items: center;
+     a{
+       align-items: center;
+       background: transparent;
+       display: flex;
+       flex-direction: column;
+       font-size: 12px;
+       font-weight: 500;
+       justify-content: center;
+       line-height: 1.5;
+       min-height: 52px;
+       min-width: 80px;
+       position: relative;
+       text-decoration: none;
+       span{
+         color: rgba(0,0,0,.6);
+         display: flex;
+         align-items: center;
+       }
+       @media screen and (max-width: 2000px) {
+         padding-right: 0;
+       }
+       @media screen and (max-width: 768px) {
+         min-width: 70px;
+       }
+     }
+     span{
+       color: rgba(0,0,0,.6);
+       display: flex;
+       align-items: center;
+     }
+
+     @media screen and (max-width: 768px) {
+       min-width: 70px;
+     }
+     &:hover,
+     &:active{
+         a{
+           span{
+             color: rgba(0,0,0,.9);
+           }
+         }
+     }
+   `;
+
+   
+   const Signout = styled.div`
+      position: absolute;
+      top: 45px;
+      background: white;
+      border-radius: 10px;
+      width: 100px;
+      height: 40px;
+      font-size: 16px;
+
+   `;
+
+   const User = styled(NavList)`
+    a > svg{
+      width: 24px;
+      border-radius: 50%;
+    }
+    a > img{
+      width: 24px;
+      height: 24px;
+      border-radius: 50%;
+    }
+    span{
+      display: flex;
+      align-items: center;
+    }
+
+    &:hover{
+      ${Signout}{
+        align-items: center;
+        display: flex;
+        justify-content: center;
+      }
+    }
+   `;
+
+   const Work = styled(User)`
+         border-left: 1px solid rgba(0,0,0,.08);
+   `;
+
 
   return (
     <Container>
@@ -104,11 +206,56 @@ const Header = () => {
         </Search>
         <Nav>
           <NavListWrap>
-            <NavList>
+            <NavList className='active'>
               <a href="">
                 <img src="/images/nav-home.svg" alt="" />
+                <span>Home</span>
               </a>
             </NavList>
+            <NavList>
+              <a href="">
+                <img src="/images/nav-jobs.svg" alt="" />
+                <span> Jobs</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a href="">
+                <img src="/images/nav-messaging.svg" alt="" />
+                <span>Chats</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a href="">
+                <img src="/images/nav-notifications.svg" alt="" />
+                <span>Nofications</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a href="">
+                <img src="/images/nav-network.svg" alt="" />
+                <span>Network</span>
+              </a>
+            </NavList>
+
+            <User>
+              <a href="">
+              <img src="/images/user.svg" alt="" />
+            <span>Me</span>
+            <img src="/images/down-icon.svg" alt="" />
+              </a>
+              <Signout>
+                <a href="">
+                  SignOut
+                </a>
+              </Signout>
+            </User>
+            <Work>
+              <a href="">
+                <img src="/images/nav-work.svg" alt="" />
+                <span>work</span>
+                <img src="/images/down-icon.svg" alt="" />
+              </a>
+            </Work>
           </NavListWrap>
         </Nav>
       </Content>
