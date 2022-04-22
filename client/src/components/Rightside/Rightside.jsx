a25926a39ee0c17e947ca9734eb520a158a79226
wilsonibekason <wanbekgithub@gmail.com>
2022-04-22 09:31:40 -0700

    working on rightside component

--- client/src/components/Rightside/Rightside.jsx
+++ client/src/components/Rightside/Rightside.jsx
@@ -23,6 +23,56 @@ const Rightside = (props) => {
     align-items: center;
     justify-content: space-between;
     font-size: 16px;
+    width: 100%;
+    color: rgba(0, 0, 0, 0.6);
+  `;
+
+  const FeedList = styled.ul`
+    margin-top: 15px;
+    li {
+      display: flex;
+      align-items: center;
+      margin: 12px;
+      position: relative;
+      font-size: 15px;
+    }
+    div {
+      display: flex;
+      flex-direction: column;
+    }
+    Button {
+      background: ${(props) => (props.primary ? "palevioletred" : "white")};
+      color: ${(props) => (props.primary ? "white" : "palevioletred")};
+      font-size: 1em;
+      margin: 1em;
+      padding: 0.25em 1em;
+      border: 2px solid palevioletred;
+      border-radius: 3px;
+    }
+  `;
+
+  const Avarter = styled.li`
+    background-image: url("https://static.expo.dev/static/brand/square-512x512.png");
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 40px;
+    height: 40px;
+    margin-right: 0px;
+  `;
+
+  const Recog = styled.a`
+    color: #0a66c2;
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  `;
+
+  const Banner = styled(FollowCard)`
+    img {
+      width: 100%;
+      height: 100%;
+    }
   `;
 
   return (
@@ -32,7 +82,37 @@ const Rightside = (props) => {
           <h2>Add to your Feed </h2>
           <img src="images/feed-icon.svg" alt="" />
         </Title>
+        <FeedList>
+          <li>
+            <a>
+              <Avarter />
+            </a>
+            <div>
+              <span>LinkedIn</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a href="">
+              <Avarter />
+            </a>
+            <div>
+              <span>LinkedIn</span>
+              <button primary>Follow</button>
+            </div>
+          </li>
+        </FeedList>
+        <Recog>
+          View all Recognition
+          <img src="img/right-icon.svg" alt="" />
+        </Recog>
       </FollowCard>
+      <Banner>
+        <img
+          src="https://images.businessnewsdaily.com/app/uploads/2022/04/04082458/1554240992.jpeg"
+          alt=""
+        />
+      </Banner>
     </Container>
   );
 };
